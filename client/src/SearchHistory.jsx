a5bb14e42eff2b39d910dd14ade089530b4f1993
Pranjala11 <pranjalameshram18@gmail.com>
2026-07-02 01:47:10 +0530

    file created

---
+++ client/src/SearchHistory.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function SearchHistory({ onCityClick }) {
+  const [history, setHistory] = useState([])
+
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const res = await axios.get('http://localhost:5000/api/history')
+        setHistory(res.data)
+      } catch (err) {
+        console.log('History error:', err)
+      }
+    }
+    fetchHistory()
+  }, [])
+
+  if (history.length === 0) return null
+
+  return (
+    <div className="history">
+      <h3>Recent Searches</h3>
+      <div className="history-list">
+        {history.map((item) => (
+          <button
+            key={item._id}
+            className="history-item"
+            onClick={() => onCityClick(item.city)}
+          >
+            🕐 {item.city}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SearchHistory
