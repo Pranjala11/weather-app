9d058f0adadcd86c59de520387012c30f6a14001
Pranjala11 <pranjalameshram18@gmail.com>
2026-07-27 18:20:25 +0530

    api key edited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
-const API_KEY = 'your_openweather_key_here'
+const API_KEY = import.meta.env.VITE_API_KEY
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
 const getWeatherIcon = (condition) => {
